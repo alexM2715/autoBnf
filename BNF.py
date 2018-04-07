ea51a4d23b043e5e904279f3dd9cc3904906c2d3
alexM2715 <alex7-23@hotmail.com>
2018-04-06 19:21:43 -0500

    archivo en python

---
+++ BNF.py
@@ -0,0 +1,26 @@
+texto = input("¿Como se llama el archivo?")
+archivo = open(texto, 'r')
+renglon = []
+for linea in archivo.readlines():
+    renglon.append(linea)
+archivo.close()
+
+nuevotexto = input("¿Como quieres que se llama el nuevo documento?") + ".txt"
+archivobnf = open(nuevotexto, 'w')
+
+for i in range(len(renglon)):
+
+
+ def mtdelse():
+
+
+  def mtdif():
+
+
+   def mtdprint():
+
+
+    def mtdwhile():
+
+
+     def mtdvariable():
